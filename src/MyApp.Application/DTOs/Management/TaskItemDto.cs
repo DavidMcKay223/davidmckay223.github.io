ad43eb53fb52004c02479b82edf470da7282ba32
dmckay <dmckay@managewaresolutions.com>
2025-02-08 22:30:04 -0700

    DM - Stubbing out Tasks

---
+++ src/MyApp.Application/DTOs/Management/TaskItemDto.cs
@@ -0,0 +1,20 @@
+﻿using MyApp.Domain.Enums.Management;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Application.DTOs.Management
+{
+    public class TaskItemDto
+    {
+        public int ID { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public TaskType TaskType { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
